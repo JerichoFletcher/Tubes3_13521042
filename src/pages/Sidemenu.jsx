fb9fa0105b78a2c720e61fb61ae319b64b4bd6c0
arleenchr <16521172@mahasiswa.itb.ac.id>
2023-05-01 17:36:12 +0700

    radio buttons, index

--- src/pages/Sidemenu.jsx
+++ src/pages/Sidemenu.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react';
 
 const Sidemenu = () => {
     return (
@@ -7,7 +8,46 @@ const Sidemenu = () => {
                 <span>+</span>
                 New Chat
             </div>
+
+            <RadioButtons />
         </aside>
+    );
+}
+
+function RadioButtons() {
+    const [selectedOption, setSelectedOption] = useState('Knuth-Morris-Pratt');
+  
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+  
+    return (
+        <div className="radio-box">
+            <div className="radio-box-kmp">
+                <label className="radio-button-kmp">
+                    <input
+                        type="radio"
+                        name="Knuth-Morris-Pratt"
+                        value="Knuth-Morris-Pratt"
+                        checked={selectedOption === 'Knuth-Morris-Pratt'}
+                        onChange={handleOptionChange}
+                    />
+                    Knuth-Morris-Pratt
+                </label>
+            </div>
+            <div className="radio-box-bm">
+                <label className="radio-button-bm">
+                    <input
+                        type="radio"
+                        name="Boyer-Moore"
+                        value="Boyer-Moore"
+                        checked={selectedOption === 'Boyer-Moore'}
+                        onChange={handleOptionChange}
+                    />
+                    Boyer-Moore
+                </label>
+            </div>
+        </div>
     );
 }
 
