01eaf28395d150d53e4be991ace312ed4df1ff6c
arleenchr <16521172@mahasiswa.itb.ac.id>
2023-05-01 16:07:43 +0700

    sidemenu, new chat button, message input box

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import logo from './resources/assets/logo.png'
 import icon_howtouse from './resources/assets/icon_howtouse.png'
 import './App.css';
+import './normal.css';
 
 function App() {
   return (
     <div className="App">
+    <aside className="App-sidemenu">
+      <div className="App-newchatbutton">
+        <span>+</span>
+        New Chat
+      </div>
+    </aside>
+    <section className="App-chatbox">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={icon_howtouse} className='App-icon-howtouse' alt="howtouse" style={{width:'3%', marginTop: '4rem', marginBottom: '1rem'}} />
-        <div style={{ 
-          backgroundColor: '#FFC4D8', 
-          padding: '1rem', 
-          paddingLeft: '3rem',
-          paddingRight: '3rem',
-          borderRadius: '1rem',
-          lineHeight: 1.0
-          }}>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 800, fontSize: 'calc(6px + 2vmin)' }}>
+        <img src={icon_howtouse} className='App-icon-howtouse' alt="howtouse" />
+        <div className="App-howtouse-text">
+          <p className="text-howtouse-title">
             How to use chwatGwiPwiTi?
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 500, fontSize: 'calc(2px + 1.75vmin)' }}>
+          <p className="text-howtouse-desc">
             Enter your question on the message box and get your answer immediately!
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 800, fontSize: 'calc(2px + 1.75vmin)' }}>
+          <p className="text-howtouse-add">
             Adding a new question and answer:
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 500, fontSize: 'calc(2px + 1.75vmin)' }}>
+          <p className="text-howtouse-add-desc">
             Enter "....................." on the message box
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 800, fontSize: 'calc(2px + 1.75vmin)' }}>
+          <p className="text-howtouse-update">
             Updating answer for a question:
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 500, fontSize: 'calc(2px + 1.75vmin)' }}>
+          <p className="text-howtouse-update-desc">
             Enter "....................." on the message box
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 800, fontSize: 'calc(2px + 1.75vmin)' }}>
+          <p className="text-howtouse-delete">
             Deleting an existing question:
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 500, fontSize: 'calc(2px + 1.75vmin)' }}>
+          <p className="text-howtouse-delete-desc">
             Enter "....................." on the message box
           </p>
-          <p style={{ fontFamily: 'Nunito', fontWeight: 400, fontSize: 'calc(1px + 1.5vmin)' }}>
+          <p className="text-howtouse-ps">
             <br />P.S: Don't forget to choose the String Matching Algorithm!
           </p>
         </div>
+        <div className="chat-input-box">
+          <textarea rows="1" className="chat-input-textarea" placeholder="Send a message...">
+
+          </textarea>
+        </div>
       </header>
+    </section>
+      
     </div>
   );
 }
