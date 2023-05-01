fb0d731ee95dbc698ef68f3062da45f875a1874f
JerichoFletcher <jericho.russel17@gmail.com>
2023-05-01 21:21:48 +0700

    test: rename tests to avoid duplicate

--- src/backend/kmp.test.js
+++ src/backend/kmp.test.js
@@ -1,48 +1,48 @@
 import kmp from "./kmp.js";
 
-test('KMP: evaluate string matching should return index where pattern first found', () => {
+test('KMP: test 1, evaluate string matching should return index where pattern first found', () => {
     const mainString = "Ini adalah itu"
     const patternString = "adalah"
     const expected = 4;
     expect(kmp(mainString, patternString)).toStrictEqual(expected);
 });
 
-test('KMP: evaluate string matching should return index where pattern first found', () => {
+test('KMP: test 2, evaluate string matching should return index where pattern first found', () => {
     const mainString = "abcdababcdabcdab"
     const patternString = "abcdabcd"
     const expected = 6;
     expect(kmp(mainString, patternString)).toStrictEqual(expected);
 });
 
-test('KMP: evaluate string matching should return index -1 (not found)', () => {
+test('KMP: test 1, evaluate string matching should return index -1 (not found)', () => {
     const mainString = "abcdababcdabcdab"
     const patternString = "abcdabca"
     const expected = -1;
     expect(kmp(mainString, patternString)).toStrictEqual(expected);
 });
 
-test('KMP: evaluate string matching should return index -1 (not found)', () => {
+test('KMP: test 2, evaluate string matching should return index -1 (not found)', () => {
     const mainString = "bacbababaabcba"
     const patternString = "ababaca"
     const expected = -1;
     expect(kmp(mainString, patternString)).toStrictEqual(expected);
 });
 
-test('KMP: evaluate string matching should return index -1 (not found)', () => {
+test('KMP: test 3, evaluate string matching should return index -1 (not found)', () => {
     const mainString = "Ini adalah itu"
     const patternString = "begitu"
     const expected = -1;
     expect(kmp(mainString, patternString)).toStrictEqual(expected);
 });
 
-test('KMP: evaluate string matching should return index -1 (not found)', () => {
+test('KMP: test 4, evaluate string matching should return index -1 (not found)', () => {
     const mainString = "Ini adalah itu"
     const patternString = "Ini aDaLah iTu"
     const expected = -1;
     expect(kmp(mainString, patternString)).toStrictEqual(expected);
 });
 
-test('KMP: evaluate string matching should return index -1 (not found)', () => {
+test('KMP: test 5, evaluate string matching should return index -1 (not found)', () => {
     const mainString = "chwatGwiPwiTi"
     const patternString = "chwatGwiPwiTi lalalala"
     const expected = -1;
