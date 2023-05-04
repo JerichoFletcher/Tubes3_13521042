b1b1b4b66d2232814d02927796fa261deaec3bca
JerichoFletcher <jericho.russel17@gmail.com>
2023-05-04 14:31:15 +0700

    backend: fix calendar

--- src/backend/calendar.js
+++ src/backend/calendar.js
@@ -1,22 +1,50 @@
+import { isNumeric } from "./num";
+
+const numOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function isValidDate(day, month, year){
+    if(typeof day !== 'number' || typeof month !== 'number' || typeof year !== 'number')return false;
+    return year > 0
+        && 1 <= month && month <= 12
+        && 1 <= day && day <= numOfDays[month-1] + ((year % 4 === 0 && month === 2) ? 1 : 0);
+}
+
 function findTheDay(date) {
+    if(typeof date !== 'string')throw new TypeError();
+
     /* Find the day of a date with Zeller's Congruence */
     let dateArr = date.split("/");
-    let day = dateArr[0];
-    let month = dateArr[1];
-    let year = dateArr[2];
-    if (month == 1 || month == 2){
+    let dayStr = dateArr[0];
+    let monthStr = dateArr[1];
+    let yearStr = dateArr[2];
+    if(!isNumeric(dayStr) || !isNumeric(monthStr) || !isNumeric(yearStr))throw new Error("Not a valid date format");
+
+    let day = parseInt(dayStr);
+    let month = parseInt(monthStr);
+    let year = parseInt(yearStr);
+    if(!isValidDate(day, month, year))throw new Error("Invalid date");
+
+    if (month === 1 || month === 2){
         month += 12;
         year--;
     }
 
+    // let q = day;
+    // let m = month;
+    // let k = year % 100;
+    // let j = year / 100;
+    // let h = q + parseInt(13 * (m+1) / 5) + k + parseInt(k / 4) + parseInt(j / 4) + 5 * j;
+    // h = h % 7;
     let q = day;
     let m = month;
-    let k = year % 100;
-    let j = parseInt(year / 100);
-    let h = q + parseInt(13 * (m+1) / 5) + k + parseInt(k / 4) + parseInt(j / 4) + 5 * j;
-    h = h % 7;
-
-    console.log(h);
+    let K = year % 100;
+    let J = Math.floor(year / 100);
+    let h = (q
+        + Math.floor(13 * (m+1) / 5)
+        + K + Math.floor(K / 4)
+        + Math.floor(J / 4)
+        - 2 * J
+    ) % 7;
     
     switch (h) {
         case 0 :
@@ -33,6 +61,8 @@ function findTheDay(date) {
             return "Thursday";
         case 6 :
             return "Friday";
+        default:
+            return "???";
     }
 }
 
--- src/backend/calendar.test.js
+++ src/backend/calendar.test.js
@@ -1,12 +1,12 @@
 import findTheDay from "./calendar.js";
 
-test('calendar: find day should return a name of a day', () => {
+test('calendar: test 1, find day should return a name of a day', () => {
     let dateTest = "04/05/2023";
     const expected = "Thursday";
     expect(findTheDay(dateTest)).toStrictEqual(expected);
 });
 
-test('calendar: find day should return a name of a day', () => {
+test('calendar: test 2, find day should return a name of a day', () => {
     let dateTest = "01/01/2012";
     const expected = "Sunday";
     expect(findTheDay(dateTest)).toStrictEqual(expected);
