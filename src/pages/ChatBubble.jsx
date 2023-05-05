d82b787a4f44d0a4f119e7d2871db2c98f48fc08
arleenchr <16521172@mahasiswa.itb.ac.id>
2023-05-05 09:22:52 +0700

    frontend: formatting scrollable areas

--- src/pages/ChatBubble.jsx
+++ src/pages/ChatBubble.jsx
@@ -58,28 +58,49 @@ const ChatBubble = () => {
         {
             history_id: 1,
             timestamp: new Date(2023, 5, 3, 10, 18, 46),
-            question: "Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
+            question: "1Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
             answer: "Lorem ipsum dolor sit amet bla bla bla stima menyenangkan wow keren bentar lagi UAS wow sebenernya ini dummy text buat ngetest bubble chat lebih dari satu baris sih wkwk",
             algorithm: "KMP"
         },
         {
             history_id: 1,
             timestamp: new Date(2023, 5, 3, 13, 18, 6),
-            question: "Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
+            question: "2Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
             answer: "Lorem ipsum dolor sit amet bla bla bla stima menyenangkan wow keren bentar lagi UAS wow sebenernya ini dummy text buat ngetest bubble chat lebih dari satu baris sih wkwk",
             algorithm: "KMP"
         },
         {
             history_id: 1,
             timestamp: new Date(2023, 5, 3, 22, 0, 11),
-            question: "Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
+            question: "3Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
             answer: "Lorem ipsum dolor sit amet bla bla bla stima menyenangkan wow keren bentar lagi UAS wow sebenernya ini dummy text buat ngetest bubble chat lebih dari satu baris sih wkwk",
             algorithm: "KMP"
         },
         {
             history_id: 1,
             timestamp: new Date(2023, 5, 3, 22, 18, 46),
-            question: "Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
+            question: "4Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
+            answer: "Lorem ipsum dolor sit amet bla bla bla stima menyenangkan wow keren bentar lagi UAS wow sebenernya ini dummy text buat ngetest bubble chat lebih dari satu baris sih wkwk",
+            algorithm: "KMP"
+        },
+        {
+            history_id: 1,
+            timestamp: new Date(2023, 5, 3, 23, 23, 23),
+            question: "5Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
+            answer: "Lorem ipsum dolor sit amet bla bla bla stima menyenangkan wow keren bentar lagi UAS wow sebenernya ini dummy text buat ngetest bubble chat lebih dari satu baris sih wkwk",
+            algorithm: "KMP"
+        },
+        {
+            history_id: 1,
+            timestamp: new Date(2023, 5, 4, 22, 18, 46),
+            question: "6Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
+            answer: "Lorem ipsum dolor sit amet bla bla bla stima menyenangkan wow keren bentar lagi UAS wow sebenernya ini dummy text buat ngetest bubble chat lebih dari satu baris sih wkwk",
+            algorithm: "KMP"
+        },
+        {
+            history_id: 1,
+            timestamp: new Date(2023, 5, 5, 22, 18, 46),
+            question: "7Mengapa apa bagaimana siapa kapan di mana ke mana aku siapa aku siapa aku siapa aing maung rawr ini dummy buat question lebih dari satu baris aja sih?",
             answer: "Lorem ipsum dolor sit amet bla bla bla stima menyenangkan wow keren bentar lagi UAS wow sebenernya ini dummy text buat ngetest bubble chat lebih dari satu baris sih wkwk",
             algorithm: "KMP"
         }
