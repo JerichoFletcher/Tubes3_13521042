61d3b3b83cac6e4d3b3952fc77a1ac3e9eef2a6c
arleenchr <16521172@mahasiswa.itb.ac.id>
2023-05-05 08:55:15 +0700

    frontend: bubble date

--- src/pages/ChatBubble.jsx
+++ src/pages/ChatBubble.jsx
@@ -2,14 +2,39 @@ import React from "react";
 
 const Bubble = ({text, type}) => {
     return (
-        <div className="bubble"
-            style={{backgroundColor: type==="Q" ? '#FB9EBD' : '#FFC4D8',
-                textAlign: "left",
-                left: type==="Q" ? '' : 0,
-                right: type==="Q" ? 0 : '',
-            }}
-        >
-            {type==="Q" ? <p>{text.question}</p> : <p>{text.answer}</p>}
+        <div className="bubble">
+            {type==="Q" ?
+                <p className="bubble-date" style={{textAlign: "right"}}>
+                    {text.timestamp.toLocaleDateString()} {text.timestamp.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+                </p>
+            :
+                null
+            }
+            {type==="Q" ?
+                <div className="bubble-text" style={{backgroundColor: type==="Q" ? '#FB9EBD' : '#FFC4D8',
+                    textAlign: "left",
+                    left: type==="Q" ? '' : 0,
+                    right: type==="Q" ? 0 : '',
+                }}>
+                    {text.question}
+                </div>
+            :
+                <div className="bubble-text" style={{backgroundColor: type==="Q" ? '#FB9EBD' : '#FFC4D8',
+                    textAlign: "left",
+                    left: type==="Q" ? '' : 0,
+                    right: type==="Q" ? 0 : '',
+                }}>
+                    {text.answer}
+                </div>
+            }
+            {type==="Q" ?
+                null
+            :
+                <p className="bubble-date" style={{textAlign: "left"}}>
+                    {text.algorithm}{'\n'}
+                    {text.timestamp.toLocaleDateString()} {text.timestamp.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+                </p>
+            }
         </div>
     );
 }
